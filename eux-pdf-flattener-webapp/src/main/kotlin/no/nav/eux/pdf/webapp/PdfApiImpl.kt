8b3c13a7df2bdcc5b24b05bbb359c74f65804e49
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2024-08-04 23:35:57 +0200

    eux-pdf-flattener

--- eux-pdf-flattener-webapp/src/main/kotlin/no/nav/eux/pdf/webapp/PdfApiImpl.kt
+++ eux-pdf-flattener-webapp/src/main/kotlin/no/nav/eux/pdf/webapp/PdfApiImpl.kt
@@ -5,7 +5,12 @@ import no.nav.eux.pdf.service.FlattenPdfService
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
+import org.springframework.http.ContentDisposition
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,6 +20,12 @@ class PdfApiImpl(
 
     @Protected
     override fun flattenPdf(file: Resource?): ResponseEntity<Resource> {
-        return ByteArrayResource(pdfService.flattenPdf(file!!.contentAsByteArray)).toOkResponseEntity()
+        val byteArrayResource = ByteArrayResource(pdfService.flattenPdf(file!!.contentAsByteArray))
+        val map: MultiValueMap<String, String> = LinkedMultiValueMap()
+        map.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
+        map.add(HttpHeaders.CONTENT_LENGTH, byteArrayResource.contentLength().toString())
+        map.add(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename(file!!.filename).build().toString())
+
+        return byteArrayResource.toPdfOkResponseEntity(map)
     }
 }
--- eux-pdf-flattener-webapp/src/main/kotlin/no/nav/eux/pdf/webapp/ResponseEntityMapper.kt
+++ eux-pdf-flattener-webapp/src/main/kotlin/no/nav/eux/pdf/webapp/ResponseEntityMapper.kt
@@ -5,8 +5,11 @@ import org.springframework.http.HttpStatus.OK
 import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
+import org.springframework.util.MultiValueMap
 
 fun <T> T.toOkResponseEntity() = ResponseEntity<T>(this, OK)
+
+fun <T> T.toPdfOkResponseEntity(map: MultiValueMap<String, String>) = ResponseEntity<T>(this, map, OK)
 
 fun <T> T.toCreatedEmptyResponseEntity() = ResponseEntity<T>(this, CREATED)
 
