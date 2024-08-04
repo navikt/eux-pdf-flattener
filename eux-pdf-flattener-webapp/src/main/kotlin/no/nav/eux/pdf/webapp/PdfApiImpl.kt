acc8ed6bcf05649e1274fb07364c996b3919bc27
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2024-08-04 21:22:33 +0200

    eux-pdf-flattener

--- eux-pdf-flattener-webapp/src/main/kotlin/no/nav/eux/pdf/webapp/PdfApiImpl.kt
+++ eux-pdf-flattener-webapp/src/main/kotlin/no/nav/eux/pdf/webapp/PdfApiImpl.kt
@@ -2,6 +2,7 @@ package no.nav.eux.pdf.webapp
 
 import no.nav.eux.pdf.openapi.api.PdfApi
 import no.nav.eux.pdf.service.FlattenPdfService
+import no.nav.security.token.support.core.api.Protected
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
 import org.springframework.http.ResponseEntity
@@ -12,6 +13,7 @@ class PdfApiImpl(
     val pdfService: FlattenPdfService
 ) : PdfApi {
 
+    @Protected
     override fun flattenPdf(file: Resource?): ResponseEntity<Resource> {
         return ByteArrayResource(pdfService.flattenPdf(file!!.contentAsByteArray)).toOkResponseEntity()
     }
