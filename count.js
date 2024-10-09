32bfb4feffa7e002e60651228b614a1ef51e5767
Knut Bjørnar Wålberg <knut.bjornar.walberg@nav.no>
2024-10-09 16:11:50 +0200

    eux-pdf-flattener

--- count.js
+++ count.js
@@ -2,7 +2,7 @@ const pdfjsLib = require('pdfjs-dist');
 
 var args = process.argv.slice(2);
 
-pdfjsLib.getDocument(args[0]).promise.then(function (doc) {
+pdfjsLib.getDocument({ url: args[0], enableXfa : true }).promise.then(function (doc) {
     var numPages = doc.numPages;
     console.log('# Document Loaded');
     console.log('Number of Pages: ' + numPages);
